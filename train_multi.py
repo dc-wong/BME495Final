e0d8e091d788b49225376c421e78ca93bb4d16f4
David C Wong <150397991+dc-wong@users.noreply.github.com>
2025-03-19 15:50:16 -0500

    improve readability

--- train_multi.py
+++ train_multi.py
@@ -1,180 +1,147 @@
-### load the torch and torchvision libraries
+### load the torch packges
 import os
 import torch
-import torchvision
-import torchvision.transforms as transforms
-
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-# import torch._dynamo
-# torch._dynamo.config.compile_backend = "nvfuser" 
 
+### load torchio for train time augmentations
+import torchio as tio
+
+### import requisite classes and functions from other files
 from loss import MultiLoss
 from model import SingleModel
 from datasets import TransformedDataset
 from metrics import MultiAccuracy
-import torchio as tio
 
-import os
-from dotenv import load_dotenv
-load_dotenv(".env", override=True)
-
-# from torch.utils.tensorboard import SummaryWriter
-
-
+### check torch and cuda version
 print(torch.__version__)
 print("cuda" if torch.cuda.is_available() else "cpu")
 print(torch.version.cuda)  # Check CUDA version
 print(torch.backends.cudnn.enabled)
 
-### from the class to the object
+### load the model into the device
 model = SingleModel()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if torch.cuda.device_count() > 1:
-    print("let's use", torch.cuda.device_count(), "GPUs")
-    model = nn.DataParallel(model)
 model.to(device)
 
 
-
-test_image = torch.rand(8, 1, 320, 320, 32).to(device)
+### run a random input of the same size as the input to check
+### that the output is the correct shape and that you do not run out of memory
+test_image = torch.rand(1, 1, 320, 320, 32).to(device)
 output = model(test_image)
 print(output.shape)
-print(output.min(), output.max())
-del output
+### clear to save ram
+del test_image, output
 torch.cuda.empty_cache()
 
+### the transformations that will be used
 transform = tio.Compose([
-    #tio.ZNormalization(include=['image']),  # Normalize intensity only on the image
+    ### Apply a random bias field on the MRI
     tio.RandomBiasField(coefficients = 0.5, order = 3, include=['image']),
-    tio.RandomAffine(scales=(0.9, 1.1), degrees=10, translation=(10, 10, 10), include=['image','label']),  # Apply only to the image
-    tio.RandomElasticDeformation(num_control_points=8, max_displacement=4, locked_borders=2, p=0.5, include=['image','label']),  # Elastic transform
-    #tio.RandomFlip(axes=(0, 1, 2), p=0.5, include=['image','label'])  # Flip along random axes
+    ### Apply random rigid transformations (rotations, translations) on both image and mask
+    tio.RandomAffine(scales=(0.9, 1.1), degrees=10, translation=(10, 10, 10), include=['image','label']),  
+    ### Apply random elastic deformations to both image and mask
+    tio.RandomElasticDeformation(num_control_points=8, max_displacement=4, locked_borders=2, p=0.5, include=['image','label']),  
 ])
 
+### Load both the training and validation sets
 trainset = TransformedDataset(image_dir = os.path.abspath("Cirrhosis_T2_3D/train_images"), label_dir = os.path.abspath("Cirrhosis_T2_3D/train_masks"),  transform=transform)
-# testset = TransformedDataset(image_dir = r"C:\PROJECTS\BME495\FinalProject\Cirrhosis_T2_3D\test_images", label_dir = r"C:\PROJECTS\BME495\FinalProject\Cirrhosis_T2_3D\test_masks",  transform=transform)
-# test_loader = torch.utils.data.DataLoader(testset, batch_size = 1, shuffle = True) # batch size is 1 because of varying sizes of MRI
 valset = TransformedDataset(image_dir = os.path.abspath("Cirrhosis_T2_3D/valid_images"), label_dir = os.path.abspath("Cirrhosis_T2_3D/valid_masks"),  transform=transform)
-
-#sampler = tio.LabelSampler(patch_size = (128, 128, 16), label_name='label', label_probabilities={0:0.05,1:0.95})
-
-#train_queue = tio.Queue(subjects_dataset=trainset, max_length=1200, samples_per_volume=4, sampler=sampler, shuffle_subjects=True, shuffle_patches=True)
-#val_queue = tio.Queue(subjects_dataset=valset, max_length=300, samples_per_volume=4, sampler=sampler, shuffle_subjects=True, shuffle_patches=True)
-#train_loader = torch.utils.data.DataLoader(train_queue, batch_size = 4)
-#train_loader = tio.SubjectsLoader(train_queue, batch_size = 16, shuffle = True)
-#val_loader = torch.utils.data.DataLoader(val_queue, batch_size = 4)
-#val_loader = tio.SubjectsLoader(val_queue, batch_size = 16, shuffle = True)
-train_loader = tio.SubjectsLoader(trainset, batch_size = 8, shuffle = True) # batch size is 1 because of varying sizes of MRI
-val_loader = tio.SubjectsLoader(valset, batch_size = 8, shuffle = True) # batch size is 1 because of varying sizes of MRI
-
-
-
+### batch size of 1 can be increased since inputs are cropped to the same size
+train_loader = tio.SubjectsLoader(trainset, batch_size = 1, shuffle = True) 
+val_loader = tio.SubjectsLoader(valset, batch_size = 1, shuffle = True) 
 dataloaders = {"train": train_loader, "val": val_loader}
 
-# just to make sure things are working correctly
+### verify data has correct information
 for batch in train_loader:
-    inputs = batch['image'][tio.DATA]  # Extract tensor from Subject
-    labels = batch['label'][tio.DATA] # Extract tensor from Subject
+    inputs = batch['image'][tio.DATA]  
+    labels = batch['label'][tio.DATA] 
+    ### check that the inputs have the correct shape and
+    ### that the minimum and maxiumum are reasonable
     print("Image batch shape:", inputs.shape)
     print("Image batch range:", inputs.min(), inputs.max())
+    ### check that the labels have the correct shape and
+    ### that the values are correct (should be 0 and 1)
     print("Label batch shape:", labels.shape)
     print("Label batch unique values:", labels.unique())
     break
 
-
-def train_model(model, criterion, optimizer, num_epochs=25, continue_training = False): #scheduler
-    # Create a temporary directory to save training checkpoints
-    best_model_params_path = os.path.join('weights', 'base_3d_unet.pt')
-    if continue_training:
-        model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
-    best_acc = float('inf')
+### create the training loop that returns the trained model
+def train_model(model, criterion, optimizer, num_epochs=25):
+    best_acc = 0.0
+    # Create a directory to save training checkpoints
+    best_model_params_path = os.path.join('weights', 'model_weights.pth')
     torch.save(model.state_dict(), best_model_params_path)
-    #scaler = torch.amp.GradScaler("cuda")
+
+    ### start the training loop
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs - 1}')
         print('-' * 10)
 
-        # Each epoch has a training and validation phase
+        ### Each epoch has a training and validation phase
         for phase in ['train', 'val']:
+            ### change the mode of the 
             if phase == 'train':
-                model.train()  # Set model to training mode
+                model.train() 
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()   
             running_loss = 0.0
             running_corrects = 0
 
-            # zero the parameter gradients
-            # optimizer.zero_grad()
-
-            # # Iterate over data.
-            # accumulation_steps = 10
+            ### Iterate over data.
             for batch_idx, batch in enumerate(dataloaders[phase]):
-                # inputs = inputs.to(device)
-                # labels = labels.to(device)
-                inputs = batch['image'][tio.DATA].float().to(device)  # Extract tensor from Subject
-                labels = batch['label'][tio.DATA].float().to(device)  # Extract tensor from Subject
+                ### load inputs and masks to the device
+                inputs = batch['image'][tio.DATA].float().to(device)  
+                labels = batch['label'][tio.DATA].float().to(device)
                 
-                # # zero the parameter gradients
+                ### zero the parameter gradients
                 optimizer.zero_grad()
 
-                # forward
-                # track history if only in train
+                ### forward pass
+                ### track gradients if we are in the train phase
                 with torch.set_grad_enabled(phase == 'train'):
-                   # with torch.autocast(device_type="cuda", dtype=torch.float16):
-
                     outputs = model(inputs)
                     loss = criterion(outputs, labels)
 
-                    # backward + optimize only if in training phase
+                    ### backward pass if we are training
                     if phase == 'train':
-                        #scaler.scale(loss).backward()  # Use scaled gradients
                         loss.backward()
-                            # optimizer.step()
-                            # if (batch_idx + 1) % accumulation_steps == 0:
-                            #     optimizer.step()  # Only step optimizer every X steps
-                        #     # zero the parameter gradients
-                        #scaler.step(optimizer)  # Step optimizer
-                        #scaler.update()
                         optimizer.step()
-                            #     optimizer.zero_grad()
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
+                ### stats
+                ### no_grad to save memory
                 with torch.no_grad():
+                    running_loss += loss.item() * inputs.size(0)
                     running_corrects += MultiAccuracy(outputs, labels.data)
+                    
+                ### free memory
                 del inputs, labels, loss, outputs
-                torch.cuda.empty_cache()  # Free unused GPU memory
+                torch.cuda.empty_cache() 
 
+            ### loss and accuracy over the entire epoch
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-
-            #if phase == 'val':
-                #scheduler.step(epoch_loss)
-                #print(scheduler.get_last_lr())
-
             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
-            # deep copy the model
-            if phase == 'val' and epoch_loss < best_acc:
+            ### save the best version of the model
+            if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_loss
                 torch.save(model.state_dict(), best_model_params_path)
-            del epoch_loss, running_loss, running_corrects #, epoch_acc
 
+            ### delete to save memory
+            del epoch_loss, running_loss, running_corrects 
         print()
-
     print(f'Best val acc: {best_acc:4f}')
-    # load best model weights
+    
+    ### load best model weights
     model.load_state_dict(torch.load(best_model_params_path))
     return model
 
 
-### Why cross entropy loss?
+### load the loss function
 criterion = MultiLoss()
-### SGD, Adam, RMSprop, ... AdamW
+### use the Adam optimizer
 optimizer = optim.Adam(model.parameters(), lr=5e-5, weight_decay=1e-2)
-### decay learning rate
-#exp_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=10, threshold= 1e-3, factor=0.999)
-
-model = train_model(model, criterion, optimizer, num_epochs=250, continue_training=False) # exp_lr_schedulr
+### Train the model
+model = train_model(model, criterion, optimizer, num_epochs=250) 
